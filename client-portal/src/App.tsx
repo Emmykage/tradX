694d7f0bda4002c9d7b813e9cdafe9050fea4b94
Lakes <lakes.ajanaku@gmail.com>
2024-01-26 11:38:47 +0100

    feat: added login page

--- client-portal/src/App.tsx
+++ client-portal/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Platform from "./pages/private/platform/Platform";
 import Lender from "./pages/private/lender/Lender";
+import SignIn from "./pages/public/signIn/SignIn";
 
 interface AppProps {}
 
@@ -8,7 +9,8 @@ const App: React.FunctionComponent<AppProps> = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Platform />} />
+        <Route path="/" element={<SignIn />} />
+        <Route path="/platform" element={<Platform />} />
         <Route path="/lender" element={<Lender />} />
       </Routes>
     </BrowserRouter>
