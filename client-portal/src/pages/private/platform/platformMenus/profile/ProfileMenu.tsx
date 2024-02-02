e0d942d73bf5a594971edd3694b949237d198f0c
MuhammadAliGabol <aligabol2002@gmail.com>
2024-02-02 05:03:29 +0500

    feat: add complete new designed profile menu

--- client-portal/src/pages/private/platform/platformMenus/profile/ProfileMenu.tsx
+++ client-portal/src/pages/private/platform/platformMenus/profile/ProfileMenu.tsx
@@ -1,34 +1,50 @@
-import { ReactNode } from "react";
 import {
-  GiftIcon,
-  LargeProfileIcon,
-  ReferralLinkIcon,
+  ArrowUpOS,
+  BookmarkIcon,
+  BoxedCheckedIcon,
+  ReloadIcon,
   SettingsIcon,
+  TooltipIcon,
 } from "../../../../../assets/icons";
 import "./profileMenu.scss";
-import { useNavigate } from "react-router-dom";
+import { Col, Row } from "antd";
 
 interface ProfileMenuProps {}
 
-const ProfileLink = ({
-  icon,
-  linkTitle,
-  link,
-}: {
-  icon: ReactNode;
-  linkTitle: string;
-  link?: string;
-}) => {
-  const navigate = useNavigate();
-  return (
-    <div className="profileLinkContainer" onClick={() => navigate(link || "")}>
-      <div className="icon">{icon}</div>
-      <p className="linkName">{linkTitle}</p>
-    </div>
-  );
-};
+// const ProfileLink = ({
+//   icon,
+//   linkTitle,
+//   link,
+// }: {
+//   icon: ReactNode;
+//   linkTitle: string;
+//   link?: string;
+// }) => {
+//   const navigate = useNavigate();
+//   return (
+//     <div className="profileLinkContainer" onClick={() => navigate(link || "")}>
+//       <div className="icon">{icon}</div>
+//       <p className="linkName">{linkTitle}</p>
+//     </div>
+//   );
+// };
 
 const ProfileMenu: React.FunctionComponent<ProfileMenuProps> = () => {
+  const infos = [
+    {
+      title: "Recieve a 50% Bonus",
+      image: "/profile/50-bonus.png",
+    },
+    {
+      title: "How to analyze the chart",
+      image: "/profile/howto-analyze.png",
+    },
+    {
+      title: "Number of the day",
+      image: "/profile/number-of-day.png",
+    },
+  ];
+
   return (
     <div>
       <div className="flexTraderProfile">
@@ -39,24 +55,76 @@ const ProfileMenu: React.FunctionComponent<ProfileMenuProps> = () => {
             <span className="id-number">12345645</span>
           </p>
         </div>
-        <div className="bigProfileIcon">
-          <LargeProfileIcon />
+        <div className="reloadIcon">
+          <ReloadIcon />
         </div>
       </div>
-      <div className="flexProfileLinks">
-        <ProfileLink
-          icon={<ReferralLinkIcon />}
-          link="/lender"
-          linkTitle="See loan offers"
-        />
-        <ProfileLink icon={<GiftIcon />} linkTitle="Boost cubes" />
+
+      <div className="traderInfoImages">
+        <Row gutter={[16, 16]} justify="start">
+          {infos.map((item, index) => (
+            <Col key={item.title + index} span={8}>
+              <img src={item.image} alt={item.title} key={index} />
+            </Col>
+          ))}
+        </Row>
       </div>
-      <button className="settings">
-        <span className="icon">
-          <SettingsIcon />
-        </span>
-        <span className="txt">Settings</span>
-      </button>
+
+      <div className="profileCard">
+        <div className="levelCard">
+          <div className="levelCardTitleContainer">
+            <ArrowUpOS />
+            <p className="levelCardTitle">Starter</p>
+          </div>
+          <div className="levelBar">
+            <p>LEVEL 1</p>
+            <p className="levelText">0/50 XP</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="statusTooltip">
+        <p>What are Statuses?</p>
+        <TooltipIcon />
+      </div>
+
+      <div className="taskCards">
+        <Row gutter={[16, 16]} justify="start">
+          <Col span={12}>
+            <div className="profileCard">
+              <div className="taskCard">
+                <div className="taskCardIcon">
+                  <BoxedCheckedIcon />
+                </div>
+                <p className="taskCardSubtitle">21:42:37</p>
+                <p className="taskCardTitle">Daily Tasks</p>
+                <p className="taskCardSubtitle">0/3</p>
+              </div>
+            </div>
+          </Col>
+          <Col span={12}>
+            <div className="profileCard">
+              <div className="taskCard">
+                <div className="taskCardIcon">
+                  <BookmarkIcon />
+                </div>
+                <p className="taskCardSubtitle"></p>
+                <p className="taskCardTitle">Achievements</p>
+                <p className="taskCardSubtitle">0/10</p>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </div>
+
+      <div className="profileCard">
+        <button className="settings">
+          <span className="icon">
+            <SettingsIcon />
+          </span>
+          <span className="txt">Settings</span>
+        </button>
+      </div>
     </div>
   );
 };
