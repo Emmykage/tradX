55c302064016d870b2deee282c4450908e944d8f
MuhammadAliGabol <aligabol2002@gmail.com>
2024-02-05 22:34:20 +0500

    feat: make tradesform mobile/tablet responsive

--- client-portal/src/components/tradeForm/TradeForm.tsx
+++ client-portal/src/components/tradeForm/TradeForm.tsx
@@ -12,66 +12,71 @@ interface TradeFormProps {}
 
 const TradeForm: React.FunctionComponent<TradeFormProps> = () => {
   return (
-    <div className="trade-form">
+    <div className="trade-form" id="tradeForm">
       <div className="coinInfo">
-        <div>
+        <div className="timeMode">
           <p className="coinTitle">EUR/USD OTC</p>
           <p className="timeSubtext">Fixed Time</p>
         </div>
         <InfoCircleIconSmall />
       </div>
 
-      <div className="amount">
-        <label htmlFor="amount">Amount, Đ</label>
-        <input type="number" name="amount" id="amounts" />
-      </div>
-
-      <div className="mathButtons">
-        <button>
-          <SubtractIcon />
-        </button>
-        <button>
-          <PlusIcon />
-        </button>
-      </div>
-
-      <div className="amount">
-        <label htmlFor="amount">Duration</label>
-        <input type="number" name="amount" id="amount" />
-      </div>
-
-      <div className="mathButtons">
-        <button>
-          <SubtractIcon />
-        </button>
-        <button>
-          <PlusIcon />
-        </button>
-      </div>
-
-      <div className="setupOrder">
-        <button>
-          <div className="buttonContent">
-            <span>Setup</span>
-            <span>Order</span>
+      <div className="amountsWrapper">
+        <div className="amountContainer">
+          <div className="amount">
+            <label htmlFor="amount">Amount, Đ</label>
+            <input type="number" name="amount" id="amounts" />
           </div>
-          <TimerIcon />
-        </button>
+
+          <div className="mathButtons">
+            <button>
+              <SubtractIcon />
+            </button>
+            <button>
+              <PlusIcon />
+            </button>
+          </div>
+        </div>
+
+        <div className="amountContainer">
+          <div className="amount">
+            <label htmlFor="amount">Duration</label>
+            <input type="number" name="amount" id="amount" />
+          </div>
+
+          <div className="mathButtons">
+            <button>
+              <SubtractIcon />
+            </button>
+            <button>
+              <PlusIcon />
+            </button>
+          </div>
+        </div>
       </div>
 
-      <div className="upNdown">
-        <button className="up">
-          <span>Up</span>
-          <span>
-            <ArrowUpRightIcon />
-          </span>
-        </button>
-        <button className="down">
-          <span>Down</span>
-          <span>
-            <ArrowDownRightIcon />
-          </span>
-        </button>
+      <div className="buttonsWrapper">
+        <div className="upNdown">
+          <button className="setupOrder">
+            <div className="buttonContent">
+              <span>Setup</span>
+              <span>Order</span>
+            </div>
+            <TimerIcon />
+          </button>
+          <button className="up">
+            <span>Up</span>
+            <span>
+              <ArrowUpRightIcon />
+            </span>
+          </button>
+          <button className="down">
+            <span>Down</span>
+            <span>
+              <ArrowDownRightIcon />
+            </span>
+          </button>
+        </div>
       </div>
       <p className="profit">Profit: + Đ0.82</p>
     </div>
