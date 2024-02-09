877eeab105c1178f3a7774a630c5d8c7e64b6b26
Muhammad <muhammad.mudassar@theaimegroup.com>
2024-02-10 04:09:20 +0500

    chore: remove unnecessary code.

--- client-portal/src/pages/private/platform/platformMenus/market/MarketMenu.tsx
+++ client-portal/src/pages/private/platform/platformMenus/market/MarketMenu.tsx
@@ -1,14 +1,8 @@
 import { useNavigate } from "react-router-dom";
 import "./marketMenu.scss";
-import {
-  ArrowRightIcon,
-  ArrowRightOS,
-  RightArrowIcon,
-} from "../../../../../assets/icons";
-import MenuListCard from "../../../../../components/menuListCard/MenuListCard";
+import { ArrowRightOS } from "../../../../../assets/icons";
 import MainItemCard from "../../../../../components/mainItemCard/MainItemCard";
 import CarouselSlider from "../../../../../components/carouselSlider/CarouselSlider";
-import BoostCubes from "../boostClubes/BoostCubes";
 
 interface MarketMenuProps {}
 
@@ -65,41 +59,6 @@ const MarketMenu: React.FunctionComponent<MarketMenuProps> = () => {
           </p>
         </MainItemCard>
       </div>
-
-      {/* <CarouselSlider>
-        <MainItemCard></MainItemCard>
-        <MainItemCard></MainItemCard>
-        <MainItemCard></MainItemCard>
-      </CarouselSlider> */}
-      {/* <BoostCubes /> */}
-      {/* <div className="carousel-wrapper">
-        <div className="flexItems">
-          <div className="text">
-            <p className="giftHead">Forex</p>
-            <p className="prof">
-              Professional tools to help predict market trends{" "}
-            </p>
-          </div>
-          <img
-            src="https://res.cloudinary.com/dt9pwfpi5/image/upload/v1703602367/bag-dynamic-color_hrvnwe.png"
-            alt="gift-bag"
-          />
-        </div>
-      </div>
-      <div className="trade-conditions">
-        <div className="imgWrap">
-          <img
-            src="https://res.cloudinary.com/dt9pwfpi5/image/upload/v1703603004/coin_g0cie0.png"
-            alt="coin"
-          />
-        </div>
-        <div className="texts">
-          <p className="tr-head">Trading Conditions</p>
-          <p className="tr-txt">
-            Features that provide more benefitial trading conditions
-          </p>
-        </div>
-      </div> */}
     </div>
   );
 };
