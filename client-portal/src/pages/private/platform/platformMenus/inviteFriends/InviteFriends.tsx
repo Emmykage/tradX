4279f76bb7b4a0c141b01c053f36a2214e1264b8
Muhammad <muhammad.mudassar@theaimegroup.com>
2024-02-10 23:21:02 +0500

    feat: Create Invite Friends Menu

---
+++ client-portal/src/pages/private/platform/platformMenus/inviteFriends/InviteFriends.tsx
@@ -0,0 +1,57 @@
+import {
+  ArrowRightOS,
+  ArrowUpOS,
+  BookmarkIcon,
+  BoxedCheckedIcon,
+  EventIcon,
+  InfoCircleIconWhite,
+  NotificationIcon2,
+  ReloadIcon,
+  SettingsIcon,
+  TooltipIcon,
+} from "../../../../../assets/icons";
+import MainItemCard from "../../../../../components/mainItemCard/MainItemCard";
+import MenuListCard from "../../../../../components/menuListCard/MenuListCard";
+import "./inviteFriends.scss";
+import { Col, Row } from "antd";
+
+interface InviteFriendsProps {}
+
+const InviteFriends: React.FunctionComponent<InviteFriendsProps> = ({}) => {
+  return (
+    <div>
+      <div className="headerExtraIcon">
+        <InfoCircleIconWhite />
+      </div>
+      <div className="InviteFriendsCon">
+        <p className="getyourreferaltext">
+          Get your referal link in just a few steps
+        </p>
+        <MainItemCard className="step1Card" variant={2}>
+          <div className="textRow">
+            <p className="titletext">Step 1 – Deposit</p>
+            <p className="subtext">Deposit money to a live account</p>
+          </div>
+          <div className="rightIconDiv">
+            <ArrowRightOS />
+          </div>
+        </MainItemCard>
+        <MainItemCard className="step1Card" variant={2}>
+          <div className="textRow">
+            <p className="titletext" style={{ color: "#969798" }}>
+              Step 1 – Deposit
+            </p>
+            <p className="subtext">Deposit money to a live account</p>
+          </div>
+        </MainItemCard>
+        <div className="textdiv">
+          <p className="referalRewardsText">REFERAL REWARDS</p>
+          <p className="megaboxText">MegaBox</p>
+        </div>
+        <MenuListCard className="listCard" title="What`s Inside?" textCenter />
+      </div>
+    </div>
+  );
+};
+
+export default InviteFriends;
