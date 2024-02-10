0f9f9fc5e159e7348157f6e9acd0b2e805625dee
MuhammadAliGabol <aligabol2002@gmail.com>
2024-02-10 05:11:40 +0500

    chore: Update events menu to include history icon for the header

--- client-portal/src/pages/private/platform/platformMenus/events/EventsMenu.tsx
+++ client-portal/src/pages/private/platform/platformMenus/events/EventsMenu.tsx
@@ -1,4 +1,8 @@
-import { ArrowRightOS, EventIcon } from "../../../../../assets/icons";
+import {
+  ArrowRightOS,
+  EventIcon,
+  HistoryIcon,
+} from "../../../../../assets/icons";
 import MainItemCard from "../../../../../components/mainItemCard/MainItemCard";
 import MenuListCard from "../../../../../components/menuListCard/MenuListCard";
 import "./eventsMenu.scss";
@@ -7,36 +11,41 @@ interface EventsMenuProps {}
 
 const EventsMenu: React.FunctionComponent<EventsMenuProps> = () => {
   return (
-    <div className="eventsContainer">
-      <div className="eventsTagLineCon">
-        <p className="eventsTagLine">
-          Get your referal link in just a few steps
-        </p>
+    <div>
+      <div className="eventHistoryIcon" onClick={() => {}}>
+        <HistoryIcon />
       </div>
-      <MenuListCard
-        title="Leagues"
-        subtitle="Join now"
-        textCenter
-        subTextCenter
-      />
-      <MainItemCard className="eventMainItemCard" variant={2}>
-        <div className="eventMainItemRow">
-          <div>
-            <EventIcon />
-            <div className="eventMainItemTextDiv">
-              <p className="happeningNowText">HAPPENING NOW</p>
-              <p className="secondText">Exclusive Trading Signals Club</p>
-              <div className="thirdTextRow">
-                <p className="endsText">Ends on</p>
-                <p className="dateText">February 26</p>
+      <div className="eventsContainer">
+        <div className="eventsTagLineCon">
+          <p className="eventsTagLine">
+            Get your referal link in just a few steps
+          </p>
+        </div>
+        <MenuListCard
+          title="Leagues"
+          subtitle="Join now"
+          textCenter
+          subTextCenter
+        />
+        <MainItemCard className="eventMainItemCard" variant={2}>
+          <div className="eventMainItemRow">
+            <div>
+              <EventIcon />
+              <div className="eventMainItemTextDiv">
+                <p className="happeningNowText">HAPPENING NOW</p>
+                <p className="secondText">Exclusive Trading Signals Club</p>
+                <div className="thirdTextRow">
+                  <p className="endsText">Ends on</p>
+                  <p className="dateText">February 26</p>
+                </div>
               </div>
             </div>
+            <div className="rightIconDiv">
+              <ArrowRightOS />
+            </div>
           </div>
-          <div className="rightIconDiv">
-            <ArrowRightOS />
-          </div>
-        </div>
-      </MainItemCard>
+        </MainItemCard>
+      </div>
     </div>
   );
 };
