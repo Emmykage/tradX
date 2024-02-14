4719b74c04395e2e8c07ef08a026b2c1d077c1df
Mudassirkz <m.mudassirkz@gmail.com>
2024-02-14 19:57:02 +0500

    chore: updated withdraw menu screen.

--- client-portal/src/pages/private/platform/platformMenus/withdraw/WithdrawMenu.tsx
+++ client-portal/src/pages/private/platform/platformMenus/withdraw/WithdrawMenu.tsx
@@ -1,75 +1,22 @@
-import {
-  ArrowRightOS,
-  InfoCircleIcon,
-  InfoCircleIconWhite,
-} from "../../../../../assets/icons";
-import MenuListCard from "../../../../../components/menuListCard/MenuListCard";
+import { UsdIcon2 } from "../../../../../assets/icons";
+import DepositCard from "../../../../../components/depositCard/DepositCard";
+import WithdrawCard from "../../../../../components/withdrawCard/WithdrawCard";
 import "./withdrawMenu.scss";
 
 interface WithdrawMenuProps {}
 
-interface ContentProps {
-  account: string;
-  amount: string;
-}
-
-interface InfoProps {
-  text: string;
-  linkText: string;
-}
-
 const WithdrawMenu: React.FunctionComponent<WithdrawMenuProps> = () => {
-  const Account: React.FC<ContentProps> = ({ account, amount }) => (
-    <div className="cardContent">
-      <div className="cardLeft">
-        <div className="cardTop">
-          <p className="cardTitle">{account}</p>
-        </div>
-        <div className="cardBottom">
-          <p className="cardSubtext">{amount}</p>
-        </div>
-      </div>
-      <div className="cardRight">
-        <ArrowRightOS width={""} height={""} />
-      </div>
-    </div>
-  );
-
-  const Info: React.FC<InfoProps> = ({ text, linkText }) => (
-    <div className="cardContent">
-      <div className="cardLeft">
-        <div className="cardTop">
-          <p className="infoText">{text}</p>
-        </div>
-        <div className="cardBottom">
-          <p className="cardLink">{linkText}</p>
-        </div>
-      </div>
-    </div>
-  );
-
   return (
     <div>
-      <div className="withdrawInfoIcon" onClick={() => {}}>
-        <InfoCircleIconWhite />
-      </div>
+      {/* <div className="withdrawTitle">Withdraw</div> */}
       <div className="withdrawMenu">
-        <MenuListCard
-          icon={<img src="/menu-images/us-flag.png" alt="USD" />}
-          customContent={
-            <Account account="From USD Account" amount="USD 0.00" />
-          }
+        <DepositCard
+          CountryIcon={<UsdIcon2 />}
+          account="From USD Account"
+          amount="USD 30"
+          icon
         />
-
-        <MenuListCard
-          icon={<InfoCircleIcon />}
-          customContent={
-            <Info
-              text="You have insufficient funds to make a withdrawal from this account"
-              linkText="Make Deposit"
-            />
-          }
-        />
+        <WithdrawCard />T
       </div>
     </div>
   );
