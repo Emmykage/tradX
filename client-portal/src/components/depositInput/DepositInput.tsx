74533b9ee8b7493824bb35feb1a85a14148feae9
Mudassirkz <m.mudassirkz@gmail.com>
2024-02-14 17:57:48 +0500

    chore: resolve spacing issue in deposit input.

--- client-portal/src/components/depositInput/DepositInput.tsx
+++ client-portal/src/components/depositInput/DepositInput.tsx
@@ -3,11 +3,7 @@ import "./DepositInput.scss";
 const DepositInput = ({ CardsIconList, placeholder }: any) => {
   return (
     <div className="deposit-Input-Wrapper">
-      <input
-        className="deposit-input"
-        type="number"
-        placeholder={placeholder}
-      />
+      <input className="deposit-input" type="text" placeholder={placeholder} />
       {CardsIconList &&
         CardsIconList.map((card: any, index: number) => (
           <div key={index} className="credit-cards">
