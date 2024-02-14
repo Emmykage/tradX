7f36e4169cb1d81829802425fa9e266210956ec9
Mudassirkz <m.mudassirkz@gmail.com>
2024-02-15 01:58:53 +0500

    chore: updated transfer menu as per figma.

--- client-portal/src/pages/private/platform/platformMenus/transfer/TransferMenu.tsx
+++ client-portal/src/pages/private/platform/platformMenus/transfer/TransferMenu.tsx
@@ -1,56 +1,44 @@
 import { Col, Row } from "antd";
-import { InfoCircleIcon, WalletIcon } from "../../../../../assets/icons";
+import {
+  DepositsIcon2,
+  InfoCircleIcon,
+  InfoCircleIconWhite,
+  UsdSquareIcon,
+} from "../../../../../assets/icons";
 import Input from "../../../../../components/input/Input";
-import MenuListCard from "../../../../../components/menuListCard/MenuListCard";
 import "./transferMenu.scss";
+import PromoCodeInput from "../../../../../components/promoCodeInpute/PromoCodeInput";
+import PaymentListItemCard from "../../../../../components/paymentListItemCard/PaymentListItemCard";
+import TransferInput from "../../../../../components/transferInput/TransferInput";
 
 interface TransferMenuProps {
   setIsRightSubDrawerContent: (value: string | null) => void;
 }
 
-const TransferMenu: React.FunctionComponent<TransferMenuProps> = ({
-  setIsRightSubDrawerContent,
-}) => {
-  const warning = true;
-
+const TransferMenu: React.FunctionComponent<TransferMenuProps> = () => {
   return (
     <div className="transferMenu">
-      <Input
-        icon={<img src="/menu-images/uk-flag.png" />}
-        className="currencyInput"
+      {/* <div className="withdrawTitle">Withdraw</div> */}
+      <TransferInput
+        subtitle="From"
+        className="promoCodeInput"
+        icon={<UsdSquareIcon />}
+        title="USD Account"
         placeholder="USD 0.00"
-        title="From: USD Account"
-        suffixIcon={<InfoCircleIcon />}
+        suffixIcon={<InfoCircleIcon stroke="#F58615" />}
       />
-
-      <MenuListCard
-        className="walletSelect"
-        title="Select the account"
-        icon={<WalletIcon />}
-        onClick={() => setIsRightSubDrawerContent("select-account")}
-      />
-
+      <PaymentListItemCard icon={<DepositsIcon2 />} title="Select Account" />
       <div className="amountInputs">
         <Row gutter={[12, 12]} justify="start">
           <Col span={12}>
-            <Input title="Amount, USD" />
+            <Input variant={1} title="Amount, USD" />
           </Col>
           <Col span={12}>
-            <Input title="Amount" />
+            <Input variant={1} title="Amount, PKR" />
           </Col>
-          {warning ? (
-            <Col span={24}>
-              <p className="warningText">
-                Transfer between these accounts is not possible
-              </p>
-            </Col>
-          ) : null}
         </Row>
       </div>
-
-      <MenuListCard title="Transfer" textCenter />
-
-      <p className="infoText">Instant money transfer</p>
+      <PaymentListItemCard title="Transfer" />
     </div>
   );
 };
