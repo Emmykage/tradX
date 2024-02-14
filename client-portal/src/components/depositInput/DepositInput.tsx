6aed7000fd7e1ae8bc4a48274bbe8ff048a9b5f8
Mudassirkz <m.mudassirkz@gmail.com>
2024-02-14 21:25:49 +0500

    chore: updated deposit input as per need.

--- client-portal/src/components/depositInput/DepositInput.tsx
+++ client-portal/src/components/depositInput/DepositInput.tsx
@@ -1,9 +1,26 @@
 import "./DepositInput.scss";
 
-const DepositInput = ({ CardsIconList, placeholder }: any) => {
+const DepositInput = ({
+  CardsIconList,
+  placeholder,
+  classname,
+  border,
+  placeholderColor,
+  marginTop,
+}: any) => {
   return (
-    <div className="deposit-Input-Wrapper">
-      <input className="deposit-input" type="text" placeholder={placeholder} />
+    <div
+      className={`deposit-Input-Wrapper ${
+        marginTop ? "marginTop" : "marginTop2"
+      } ${border ? "border" : "border2"} ${classname}`}
+    >
+      <input
+        className={`deposit-input ${
+          placeholderColor ? "placeholderColor" : "placeholderColor2"
+        }`}
+        type="text"
+        placeholder={placeholder}
+      />
       {CardsIconList &&
         CardsIconList.map((card: any, index: number) => (
           <div key={index} className="credit-cards">
