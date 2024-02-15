2d5e296fd2eba4cddd1b3aac1d7907f4758f545f
MuhammadAliGabol <aligabol2002@gmail.com>
2024-02-15 07:07:43 +0500

    chore: add graph in TheCharts page on step 3 of walkthrough page

--- client-portal/src/pages/private/platform/WalkThrough/steps/TheCharts.tsx
+++ client-portal/src/pages/private/platform/WalkThrough/steps/TheCharts.tsx
@@ -1,11 +1,24 @@
+import { useEffect, useState } from "react";
+import ChartComponent, { DataPoint } from "../components/WalkthroughChart";
+import { initialData } from "../data/initialGraphData";
+
 interface TheChartsProps {
   className: string;
   setStep: React.Dispatch<React.SetStateAction<number>>;
 }
 
 const TheCharts: React.FC<TheChartsProps> = ({ className, setStep }) => {
+  const [graphData, setGraphData] = useState<DataPoint[]>([]);
+
+  useEffect(() => {
+    setGraphData(initialData);
+  }, []);
+
   return (
     <div className={`walkthroughStep theChartsStep ${className}`}>
+      <div className="graphContainerWalkthrough">
+        {graphData?.length && <ChartComponent data={graphData} />}
+      </div>
       <img className="euroUsdButton" src="/walkthrough/eur-usd-btn.png" />
       <p className="walkthroughSubtext">
         The charts shows how the price of an assets changes. If the line on the
