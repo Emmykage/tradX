06d29b6face2bf894ac395740b194d80ce6d352a
MuhammadAliGabol <aligabol2002@gmail.com>
2024-02-16 01:11:53 +0500

    feat: generate time in seconds in 2mins of span for initial chart data

--- client-portal/src/pages/private/platform/WalkThrough/data/initialGraphData.ts
+++ client-portal/src/pages/private/platform/WalkThrough/data/initialGraphData.ts
@@ -1,25 +1,30 @@
-export const initialData = generateRandomData(50, "2018-12-22", 32.51, 0.1);
+export const initialData = generateRandomData(
+  120,
+  "2024-02-01T17:52:00.000Z",
+  1.164,
+  0.01
+);
 
 function generateRandomData(
   count: number,
   startTime: string,
   startValue: number,
   volatility: number
-): { time: string; value: number }[] {
+): { time: number; value: number }[] {
   const data = [];
-  let currentDate = new Date(startTime);
+  let currentTimestamp = new Date(startTime).getTime() / 1000; // Convert to seconds
   let currentValue = startValue;
 
   for (let i = 0; i < count; i++) {
-    currentDate.setDate(currentDate.getDate() + 1);
-
     const randomMovement = (Math.random() - 0.5) * volatility;
     currentValue += randomMovement;
 
     data.push({
-      time: currentDate.toISOString().split("T")[0],
+      time: currentTimestamp,
       value: Math.max(currentValue, 0), // Ensure the value is non-negative
     });
+
+    currentTimestamp += 60; // Add 60 seconds for each data point
   }
 
   return data;
