d315270a5c55073e0d5323a0daa932eda3097897
Mudassirkz <m.mudassirkz@gmail.com>
2024-02-16 19:41:04 +0500

    chore: updated and responsive navbar of download.

--- client-portal/src/pages/public/downloads/Download.tsx
+++ client-portal/src/pages/public/downloads/Download.tsx
@@ -7,15 +7,23 @@ import {
 } from "../../../assets/icons";
 import { Col, Row, Typography } from "antd";
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Download = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  const handleNavigationToggle = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
   return (
     <div className="downloads">
-      <nav>
+      <nav className={`${toggleMenu ? "nav-top" : ""}`}>
         <div className="nav-items-start">
           <p>Movement</p>
           <p>Bills</p>
         </div>
+
         <div className="nav-items-end">
           <p>Support</p>
           <SearchIcon />
@@ -23,7 +31,32 @@ const Download = () => {
           <NotificationIcon3 />
           <ProfileIcon2 />
         </div>
+        {toggleMenu && (
+          <div className="mobile-nav-items">
+            <p>Support</p>
+            <SearchIcon />
+            <EyeIcon />
+            <NotificationIcon3 />
+            <ProfileIcon2 />
+          </div>
+        )}
+        {!toggleMenu ? (
+          <div onClick={handleNavigationToggle} className="hamburger-icon">
+            <div className="ham-line-1"></div>
+            <div className="ham-line-2"></div>
+            <div className="ham-line-3"></div>
+          </div>
+        ) : (
+          <div
+            onClick={handleNavigationToggle}
+            className="hamburger-icon-cross"
+          >
+            <div className="ham-cross-1"></div>
+            <div className="ham-cross-2"></div>
+          </div>
+        )}
       </nav>
+
       <div className="downloads-links">
         <Typography.Title className="download-links-version">
           Download the right version for you
