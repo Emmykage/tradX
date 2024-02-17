ab32e661d08d5518d73203847236c471c6430407
MuhammadAliGabol <aligabol2002@gmail.com>
2024-02-17 07:18:47 +0500

    fix: status error for Status handler

--- client-portal/src/pages/private/transactions/helpers.tsx
+++ client-portal/src/pages/private/transactions/helpers.tsx
@@ -2,23 +2,25 @@ import { FC, ReactNode } from "react";
 import { BankCardIcon } from "../../../assets/icons";
 import { MethodsType } from "./types";
 
-export function methodIconHandler(method: MethodsType): ReactNode | null{
-    switch(method){
-        case "Bank Card":
-            return (
-              <div style={{
-                display: "flex",
-                alignItems: "center"
-              }}>
-                <BankCardIcon />
-                <span style={{marginLeft: "8px"}}>Bank Card</span>
-              </div>
-            );
-        default:
-            return null;
-    }
+export function methodIconHandler(method: MethodsType): ReactNode | null {
+  switch (method) {
+    case "Bank Card":
+      return (
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          <BankCardIcon />
+          <span style={{ marginLeft: "8px" }}>Bank Card</span>
+        </div>
+      );
+    default:
+      return null;
+  }
 }
 
-export const StatusHandler:FC<{status: "success" | "failed" | "pending"}>=({status}) => (
-    <p status={status} className="tb-td-transaction-status">{status}</p>
-);
+export const StatusHandler: FC<{
+  status: "success" | "failed" | "pending";
+}> = ({ status }) => <p className="tb-td-transaction-status">{status}</p>;
