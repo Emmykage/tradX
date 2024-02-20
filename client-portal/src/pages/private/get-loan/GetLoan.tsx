8b47b40b14137c60b299a33fb576e4cd0b10b3b7
Mudassirkz <m.mudassirkz@gmail.com>
2024-02-21 02:37:55 +0500

    feat: created verification tab in get a loan screen.

--- client-portal/src/pages/private/get-loan/GetLoan.tsx
+++ client-portal/src/pages/private/get-loan/GetLoan.tsx
@@ -1,12 +1,12 @@
-import LoanPersonalData from "../../../components/loanPersonalData/LoanPersonalData";
-import LoanRegister from "../../../components/loanRegister/LoanRegister";
+import LoanIdentification from "../../../components/loanIdentification/LoanIdentification";
+// import LoanPersonalData from "../../../components/loanPersonalData/LoanPersonalData";
+// import LoanRegister from "../../../components/loanRegister/LoanRegister";
 import "./getLoan.scss";
 
 const GetLoan = () => {
   return (
     <div>
-      {/* <LoanRegister /> */}
-      <LoanPersonalData />
+      <LoanIdentification />
     </div>
   );
 };
