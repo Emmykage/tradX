5c7803415e754f7b6805b26697509406786531d8
MuhammadAliGabol <aligabol2002@gmail.com>
2024-02-21 07:30:26 +0500

    feat: add feature to select dynamic images from and show dynamically in the background of graph

--- client-portal/src/components/backgroundImageSelector/BackgroundImageSelector.tsx
+++ client-portal/src/components/backgroundImageSelector/BackgroundImageSelector.tsx
@@ -1,36 +1,49 @@
+import React, { useEffect, useState } from "react";
 import { Col, Row } from "antd";
-import "./backgroundImageSelector.scss";
 import { EyeSlashIcon } from "../../assets/icons";
+
+import { BackgroundImage, backgroundImages } from "./imagesData";
+import "./backgroundImageSelector.scss";
+import { setAppearanceBackground } from "../../pages/lib/utils";
 
 interface BackgroundImageSelectorProps {}
 
 const BackgroundImageSelector: React.FunctionComponent<
   BackgroundImageSelectorProps
 > = () => {
-  const images = [
-    { url: "", alt: "no-image" },
-    { url: "/background-images/background-1.jpg", alt: "background-image" },
-    { url: "/background-images/background-2.jpg", alt: "background-image" },
-    { url: "/background-images/background-3.jpg", alt: "background-image" },
-    { url: "/background-images/background-4.jpg", alt: "background-image" },
-    { url: "/background-images/background-5.jpg", alt: "background-image" },
-    { url: "/background-images/background-6.jpg", alt: "background-image" },
-    { url: "/background-images/background-7.jpg", alt: "background-image" },
-    { url: "/background-images/background-8.jpg", alt: "background-image" },
-  ];
+  const [images, setImage] = useState<BackgroundImage[] | []>([]);
+
+  const storedImageId = localStorage.getItem("selectedBackgroundImage");
+  const selectedImageId = storedImageId ? parseInt(storedImageId) : "0";
+
+  useEffect(() => {
+    setImage(backgroundImages);
+  }, []);
+
+  const handleImageClick = (imageId: number) => {
+    setAppearanceBackground(imageId.toString());
+  };
+
   return (
     <div className="backgroundImageSelector">
       <p className="selectorTitle">Background image</p>
       <div className="imageList">
         <Row gutter={[12, 12]} justify="start">
-          {images?.map((image, index) => (
-            <Col span={8} sm={4} md={8} key={image.alt + index}>
-              <div className="imageContainer">
-                {image?.url ? (
-                  <img src={image.url} alt={image.alt} />
-                ) : (
-                  <EyeSlashIcon />
-                )}
+          {images?.map((image) => (
+            <Col span={8} sm={4} md={8} key={image.id}>
+              <div
+                className={`imageContainer ${
+                  selectedImageId === image.id ? "active" : ""
+                }`}
+                onClick={() => handleImageClick(image.id)}
+              >
+                <div className="cardInner">
+                  {image.url ? (
+                    <img src={image.url} alt={image.alt} />
+                  ) : (
+                    <EyeSlashIcon />
+                  )}
+                </div>
               </div>
             </Col>
           ))}
