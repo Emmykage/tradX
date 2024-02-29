ac1d1762afdc6c8c5efd0788b9bb795ffb05455d
MuhammadAliGabol <aligabol2002@gmail.com>
2024-02-29 08:09:27 +0500

    feat: create transaction API's hook to integrate with transactions

---
+++ client-portal/src/api/wallet/useTransactions.ts
@@ -0,0 +1,60 @@
+import { useMutation } from "@tanstack/react-query";
+import { ITransaction } from "@interfaces";
+import getEnv from "utils/env";
+
+type useTransactionsProps = {
+  onSuccess?: (
+    data: ITransaction[],
+    variables: unknown,
+    context: unknown
+  ) => void;
+  onError?: (error: unknown, variables: unknown, context: unknown) => void;
+  [index: string]: any;
+};
+
+export async function fetchTransactions(token: string): Promise<any> {
+  const BASE_URL = getEnv("VITE_API_BASE_URL");
+  try {
+    const response = await fetch(`${BASE_URL}/wallet/transactions/`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const result = await response.json();
+
+    if (!response.ok) {
+      throw new Error(`${result}`);
+    }
+    return result;
+  } catch (error) {
+    throw new Error(error as string);
+  }
+}
+
+export const useTransactions = (props: useTransactionsProps) => {
+  const receivedProps = props || ({} as useTransactionsProps);
+
+  const {
+    onSuccess: onSuccessOverride,
+    onError: onErrorOverride,
+    ...rest
+  } = receivedProps;
+
+  return useMutation<any, unknown, any>({
+    mutationFn: (token: string) => fetchTransactions(token),
+    onSuccess: (data, variables, context) => {
+      if (onSuccessOverride) {
+        onSuccessOverride(data, variables, context);
+      }
+    },
+    onError: (error, variables, context) => {
+      if (onErrorOverride) {
+        onErrorOverride(error, variables, context);
+      }
+    },
+    ...(rest || {}),
+  });
+};
+
+export default useTransactions;
--- client-portal/src/shared/interfaces/index.ts
+++ client-portal/src/shared/interfaces/index.ts
@@ -2,3 +2,4 @@ export type { default as IUser } from "./IUser";
 export type { default as ISignInForm } from "./ISignInForm";
 export type { default as IWallet } from "./IWallet";
 export type { default as IWalletType } from "./IWalletType";
+export type { default as ITransaction } from "./ITransaction";
