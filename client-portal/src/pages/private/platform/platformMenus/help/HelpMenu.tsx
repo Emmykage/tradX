2e89f3df5995453b3894e0088e4ffd0bf5f851ff
Mudassirkz <m.mudassirkz@gmail.com>
2024-03-03 01:38:30 +0500

    feat: created ai and news menus and updated help menu as per  figma design.

--- client-portal/src/pages/private/platform/platformMenus/help/HelpMenu.tsx
+++ client-portal/src/pages/private/platform/platformMenus/help/HelpMenu.tsx
@@ -1,11 +1,8 @@
 import { Dispatch, ReactNode, SetStateAction } from "react";
 import "./helpMenu.scss";
-import {
-  GraduateIcon,
-  HelpIcon,
-  SupportChartIcon,
-} from "../../../../../assets/icons";
+import { VideoIcon } from "../../../../../assets/icons";
 import { LeftSubDrawer } from "../../types";
+import { Col, Row } from "antd";
 
 interface HelpMenuProps {
   setLeftSubDrawer: Dispatch<SetStateAction<LeftSubDrawer>>;
@@ -32,6 +29,33 @@ const HelpLink = ({
   );
 };
 
+const HelpMenuList = [
+  {
+    id: 1,
+    title: "Discover the Platform",
+    time: "02:01",
+    img: "/menu-images/svgs/ScholarHat.svg",
+  },
+  {
+    id: 2,
+    title: "Opening Trades",
+    time: "02:46",
+    img: "/menu-images/svgs/TradingSignalsSky.svg",
+  },
+  {
+    id: 3,
+    title: "Making Forecasts",
+    time: "04:46",
+    img: "/menu-images/svgs/SearchBigImage.svg",
+  },
+  {
+    id: 4,
+    title: "Deposit & Withdrawal",
+    time: "04:46",
+    img: "/menu-images/svgs/DollarSignSky.svg",
+  },
+];
+
 const HelpMenu: React.FunctionComponent<HelpMenuProps> = ({
   setLeftSubDrawer,
   setIsLeftSubDrawerOpen,
@@ -40,29 +64,62 @@ const HelpMenu: React.FunctionComponent<HelpMenuProps> = ({
     <div className="helpMenu">
       <div className="flexHelpLnks">
         <HelpLink
-          icon={<HelpIcon />}
+          icon={<img src="/menu-images/svgs/SupportMenuIcon.svg" />}
           heading="Support"
-          text="Ask a question"
+          text="We’re here for you 24/7"
           onClick={() => {
             setLeftSubDrawer("support");
             setIsLeftSubDrawerOpen(true);
           }}
         />
         <HelpLink
-          icon={<GraduateIcon />}
+          icon={<img src="/menu-images/svgs/HelpCenterMenuIcon.svg" />}
           heading="Help Center"
-          text="Find answers"
+          text="Get to know the platform"
           onClick={() => {
             setLeftSubDrawer("help-center");
             setIsLeftSubDrawerOpen(true);
           }}
         />
       </div>
-      <HelpLink
-        icon={<SupportChartIcon />}
-        heading="Support"
-        text="Get to know our trading modes"
-      />
+      <div className="flexHelpLnks">
+        <HelpLink
+          icon={<img src="/menu-images/svgs/EducationMenuIcon.svg" />}
+          heading="Education"
+          text="Expand your knowledge"
+          onClick={() => {
+            setLeftSubDrawer("support");
+            setIsLeftSubDrawerOpen(true);
+          }}
+        />
+        <HelpLink
+          icon={<img src="/menu-images/svgs/TradingTutorialsIcon.svg" />}
+          heading="Trading Tutorials"
+          text="Learn how to open a trade"
+          onClick={() => {
+            setLeftSubDrawer("help-center");
+            setIsLeftSubDrawerOpen(true);
+          }}
+        />
+      </div>
+      <div className="videosContainer">
+        <h2 className="videoLessons">Video Lessons</h2>
+        {HelpMenuList.map((item) => (
+          <div key={item.id} className="videosItems">
+            <Row className="videoItemRow">
+              <Col span={14} className="videoItemCol1">
+                <div className="videoItemHeader">
+                  <VideoIcon /> {item.time}
+                </div>
+                <h2>{item.title}</h2>
+              </Col>
+              <Col span={10} className="videoItemImage">
+                <img src={item.img} alt="" />
+              </Col>
+            </Row>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
