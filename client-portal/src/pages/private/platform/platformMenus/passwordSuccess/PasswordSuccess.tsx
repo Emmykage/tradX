422cffc4fd60f7ce176e8b1955b51e866a040bd8
Mudassirkz <m.mudassirkz@gmail.com>
2024-03-05 18:58:28 +0500

    chore: removed cross icon and updated arrow slider component.

--- client-portal/src/pages/private/platform/platformMenus/passwordSuccess/PasswordSuccess.tsx
+++ client-portal/src/pages/private/platform/platformMenus/passwordSuccess/PasswordSuccess.tsx
@@ -1,6 +1,5 @@
-import { CloseIcon, SuccessIcon } from "../../../../../assets/icons";
+import { SuccessIcon } from "../../../../../assets/icons";
 import { Typography } from "antd";
-import PaymentListItemCard from "../../../../../components/paymentListItemCard/PaymentListItemCard";
 import "./PasswordSuccess.scss";
 import { Dispatch, SetStateAction } from "react";
 import { RightSubDrawerContent } from "../../types";
@@ -16,9 +15,6 @@ const PasswordSuccess: React.FunctionComponent<PasswordSuccessProps> = ({
 }) => {
   return (
     <div className="password-success">
-      <div className="closeIcon">
-        <CloseIcon />
-      </div>
       <SuccessIcon width="40" height="40" />
       <Typography.Title className="success-message">
         Your Password has been modified!
