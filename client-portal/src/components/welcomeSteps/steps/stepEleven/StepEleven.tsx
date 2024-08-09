871bf824c753181b560bee067fc2dd60569b1772
mTayo <m.adinlewa@gmail.com>
2024-08-09 21:11:30 +0100

    conflict fixed

--- client-portal/src/components/welcomeSteps/steps/stepEleven/StepEleven.tsx
+++ client-portal/src/components/welcomeSteps/steps/stepEleven/StepEleven.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './stepEleven.scss'
+import './StepEleven.scss'
 import TestWalkThrough from 'components/welcomeSteps/components/modal/TestWalkThrough';
 import CustomMarkerOne from 'components/welcomeSteps/components/customMarkerOne/CustomMarkerOne';
 import CustomMarkerTwo from 'components/welcomeSteps/components/customMarkerTwo/CustomMarkerTwo';
