7c42faa32e5ef477200006eb0336504ea8964b6d
Abenezer Maregu <99409488+Drabsenior@users.noreply.github.com>
2024-08-09 19:57:27 +0300

    refactor: removed langIcon

--- client-portal/src/pages/public/home/commonComponents/navbar/Navbar.tsx
+++ client-portal/src/pages/public/home/commonComponents/navbar/Navbar.tsx
@@ -1,10 +1,7 @@
 import './navbar.scss'
 import TradxLogo from '../../../../../assets/home/tradxlogo.png'
-import ENIcon from '../../../../../assets/home/langicon.png'
 
-
-
-
+import { MouseEvent } from 'react';
 import { ArrowDownOS, MenuBar, MenuCloseIcon, SearchIcon } from 'assets/icons'
 import { useEffect, useRef, useState } from 'react'
 import { localFlagHandler } from 'i18n/helpers'
@@ -70,11 +67,13 @@ const Navbar= () => {
     console.log(localFlagHandler(countryCode.toLocaleLowerCase()))
     console.log(countryCode.toLocaleLowerCase());
     const languageSelectorRef = useRef()
-    const handleClickOutside = (event) => {
-        if (languageSelectorRef.current && !languageSelectorRef.current?.contains(event.target)) {
-          setToggleLanguageSelector(false);
-        }
-      };
+
+    const handleClickOutside = (event: MouseEvent<Document>) => {
+      if (languageSelectorRef.current && !languageSelectorRef.current.contains(event.target as Node)) {
+        setToggleLanguageSelector(false);
+      }
+    };
+    
     
       useEffect(() => {
         document.addEventListener('mousedown', handleClickOutside);
@@ -188,25 +187,29 @@ const Navbar= () => {
             {/* language selector */}
             <div className='languageSelectorContainer'>
                 <div className='languageButton' onClick={()=>setToggleLanguageSelector(!toggleLanguageSelector)}>
-                    <img src={ENIcon} alt="" />
+                    <img src={localFlagHandler('en')} alt="" />
                     <h2>En</h2>
                     <ArrowDownOS height="15" width="10"/>
                 </div>
                 <div className={`languageDropDownMenu ${toggleLanguageSelector ? 'showLanguageDropDown': 'closeLanguageDropDown'}`}>
                     <div className='languageValue'>
-                    <img src={ENIcon} alt="" />
+                    <img src={localFlagHandler('en')} alt="" />
                     <h2>English</h2> 
                     </div>
                     <div className='languageValue'>
-                    <img src={ENIcon} alt="" />
+                    <img src={localFlagHandler('es')} alt="" />
                     <h2>Spanich</h2> 
                     </div>
                     <div className='languageValue'>
-                    <img src={ENIcon} alt="" />
+                    <img src={localFlagHandler('ar')} alt="" />
                     <h2>Arabic</h2> 
                     </div>
                     <div className='languageValue'>
-                    <img src={ENIcon} alt="" />
+                    <img src={localFlagHandler('jp')} alt="" />
+                    <h2>Japanese</h2> 
+                    </div>
+                    <div className='languageValue'>
+                    <img src={localFlagHandler('hi')} alt="" />
                     <h2>Mandarin</h2> 
                     </div>
                 </div>
