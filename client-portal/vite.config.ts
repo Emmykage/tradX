9c5f2a155a0e3778a1d170055aae003204a09d69
root <jordanoswork2021@gmail.com>
2024-08-10 00:26:11 +0200

    build: vite multiple environment build

--- client-portal/vite.config.ts
+++ client-portal/vite.config.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  envDir: './env',
   plugins: [react(), tsconfigPaths()],
   server: {
     host: true,
