e98e76b8ec8ca9f3baa0b5a87694427017e3e90e
morris menanya <emmiemenz@gmail.com>
2024-08-15 11:08:38 +0100

    add translation files

--- client-portal/src/pages/public/trading/platform/landing/Landing.tsx
+++ client-portal/src/pages/public/trading/platform/landing/Landing.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import "./landing.scss"
+import { useTranslation } from 'react-i18next'
 
 const Landing = () => {
+  const {t, i18n} = useTranslation(["translation", "common"])
+  console.log(i18n, "heyyyy")
   return (
     <div className='landingContainer'>
         <div className='navRouting'>
         </div>
         <h1>Trading Platform</h1>
-        <p>Take your trading to the next level with the markets.com powerful, feature & benefit rich platform, featuring high grade charting, unique analytic tools and customisable alerts.</p>
+        <p>{t("tradingIntro")}</p>
+        {/* <p>Take your trading to the next level with the markets.com powerful, feature & benefit rich platform, featuring high grade charting, unique analytic tools and customisable alerts.</p> */}
      </div>
 
   )
