92c1b247d4875676f97105f45c9497c2874ba12b
morris menanya <emmiemenz@gmail.com>
2024-08-21 15:53:15 +0100

    update KYC document page

--- client-portal/src/components/fileInput/fileInput.tsx
+++ client-portal/src/components/fileInput/fileInput.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import type { UploadProps } from 'antd';
-import { message, Upload, Progress } from 'antd';
-import './input.scss';
+import { message, Upload, Button, Progress } from 'antd';
 import { FileAttatchment, FIleIcon } from 'assets/icons';
+import './input.scss';
+
 const { Dragger } = Upload;
 
 interface FileInputProps {
@@ -13,24 +14,45 @@ const FileInput: React.FC<FileInputProps> = ({ handleChange }) => {
   const [uploadProgress, setUploadProgress] = useState<number | null>(null); 
   const [isSizeLimitExceeded, setIsSizeLimitExceeded] = useState(false);
   const [fileInfo, setFileInfo] = useState<{ name: string, size: number } | null>(null);
+console.log(uploadProgress)
+  const props: UploadProps = {
+    name: 'file',
+    multiple: false,
+    showUploadList: false,
+    
+    beforeUpload(file) {
+      const fileSizeMB = file.size / 1024 / 1024; 
+      if (fileSizeMB > 3) {
+        setIsSizeLimitExceeded(true);
+        message.error(`File size exceeds 3MB. File size: ${Math.round(fileSizeMB)}MB`);
+        return false; 
+      }
+
+      setIsSizeLimitExceeded(false);
+      simulatedUpload(file);
+
+      const formData = new FormData();
+      formData.append("file", file);
+      handleChange(formData);
+      setFileInfo({ name: file.name, size: Math.round(file.size / 1024) }); 
+      return false; 
+    },
+    
+    onRemove: () => {
+      setFileInfo(null); // Clear file info on removal
+    }
+  };
 
   const simulatedUpload = (file: File) => {
     const fileSizeKB = Math.round(file.size / 1024);
-    setFileInfo({ name: file.name, size: fileSizeKB });
     console.log("Starting simulated upload");
 
     const totalSize = fileSizeKB;
-    const uploadSpeed = 600; // KB/s
-    const totalUploadTimeMs = (totalSize / uploadSpeed) * 1000;
+    const uploadSpeed = 600; 
     const updateIntervals = 100; // ms
 
     let progress = 0;
 
-    if (fileSizeKB > 3000) {
-      message.error("File size exceeds 3MB");
-      setIsSizeLimitExceeded(true);
-      return;
-    }
 
     const interval = setInterval(() => {
       if (progress < 100) {
@@ -38,37 +60,19 @@ const FileInput: React.FC<FileInputProps> = ({ handleChange }) => {
         setUploadProgress(Math.min(progress, 100));
       } else {
         clearInterval(interval);
-        setUploadProgress(null);
+        setUploadProgress(100);
         message.success(`${file.name} file uploaded successfully.`);
       }
     }, updateIntervals);
-  };
 
-  const props: UploadProps = {
-    name: 'file',
-    multiple: false,
-    showUploadList: false,
-    beforeUpload(file) {
-      const fileSizeMB = file.size / 1024 / 1024; 
-      if (fileSizeMB > 2) {
-        setIsSizeLimitExceeded(true);
-        message.error(`File size exceeds 3MB. File size: ${Math.round(fileSizeMB)}MB`);
-        return false; 
-      }
 
-      setIsSizeLimitExceeded(false); // Reset size limit flag if within limit
-      simulatedUpload(file);
-
-      const formData = new FormData();
-      formData.append("file", file);
-      handleChange(formData);
-      // return false;
-    }
+    setUploadProgress(100)
   };
 
   return (
     <div className="fileInput max-w-md">
-      {uploadProgress !== null && !isSizeLimitExceeded ? (
+      {fileInfo && !isSizeLimitExceeded ? (
+        <>
         <div className='progress-wrapper border-2 border-[#0094FF]'>
           <div className='text-wrap flex gap-5 items-center'>
             <p className="ant-upload-drag-icon">
@@ -76,26 +80,41 @@ const FileInput: React.FC<FileInputProps> = ({ handleChange }) => {
             </p>
             <div>
               <p className='text-[#A3A8B0] flex-1 break-words text-wrap max-w-sm text-ellipsis whitespace-nowrap overflow-hidden'>
-                {fileInfo?.name}
+                {fileInfo.name}
               </p>
               <p className='text-[#A3A8B0]'>
-                {fileInfo?.size}KB
+                {fileInfo.size}KB
               </p>
             </div>
           </div>
           <div className='text-wrap ant-upload-text h-2 flex-1 my-2'>
+            {uploadProgress != null && (
+
             <Progress
               percent={Math.round(uploadProgress)}
               status="active"
               showInfo={true}
               strokeColor="#1890ff"
             />
+          )}
+
           </div>
+       
         </div>
-      ) : (
+           <div className='mt-2 flex justify-between items-center'>
+            <span className='text-[#0094FF]'>
+            Change File
+
+            </span>
+            <span className='text-[#E03137]'  onClick={() => setFileInfo(null)}>
+              Remove file
+            </span>
+          </div>
+        </>
+      ) : isSizeLimitExceeded ? (
         <div>
           <Dragger
-            style={{ border: isSizeLimitExceeded ? "1px dashed red" : undefined }}
+            style={{ border: "2px dashed red" }}
             className='fail'
             {...props}
           >
@@ -108,19 +127,32 @@ const FileInput: React.FC<FileInputProps> = ({ handleChange }) => {
                   Attach or drag/drop your documents
                 </p>
                 <p className="ant-upload-hint text-white text-left">
-                  .PDF, .DOCX, PNG, JPG (max. 2mb)
+                  .PDF, .DOCX, PNG, JPG (max. 3MB)
                 </p>
               </div>
             </div>
           </Dragger>
-
-          {/* Display error message if the file size is too big */}
-          {isSizeLimitExceeded && (
-            <p className="error-message pr-10 max-w-sm text-red-500">
-              The file size is too big. Please upload a file smaller than 3MB.
-            </p>
-          )}
+          <p className="text-red-500 max-w-sm">The file size is too big. Please, upload
+          a maximum 2 MB size.</p>
         </div>
+      ) : (
+        <Dragger
+          {...props}
+        >
+          <div>
+            <p className="ant-upload-drag-icon">
+              <FileAttatchment />
+            </p>
+            <div className="text-wrap">
+              <p className="ant-upload-text text-white text-left">
+                Attach or drag/drop your documents
+              </p>
+              <p className="ant-upload-hint text-white text-left">
+                .PDF, .DOCX, PNG, JPG (max. 3MB)
+              </p>
+            </div>
+          </div>
+        </Dragger>
       )}
     </div>
   );
