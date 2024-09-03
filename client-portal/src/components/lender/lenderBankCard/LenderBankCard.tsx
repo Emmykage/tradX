1256e5430bb2ff6f0f2285cc7ef9238b39e28839
Menanya Morris <97867917+Emmykage@users.noreply.github.com>
2024-09-03 19:42:32 +0100

    add components to m

--- client-portal/src/components/lender/lenderBankCard/LenderBankCard.tsx
+++ client-portal/src/components/lender/lenderBankCard/LenderBankCard.tsx
@@ -1,27 +1,52 @@
-import { BlueEllipse, YellowEllipse } from "../../../assets/icons";
+import {  BottomEllipse, TopEllipse } from "../../../assets/icons";
 import "./lenderBankCard.scss";
 
 interface LenderBankCardProps {
   name?: string;
   image?: string;
   small?: boolean;
+  topElipseBottom?: string;
+  topElipseright?: string;
+  topElipseLeft?: string;
+  topElipseTop?: string;
+  bottomElipseBottom?: string;
+  bottomElipseright?: string;
+  bottomElipseLeft?: string;
+  bottomElipseTop?: string;
+  topColor?: string;
+  topDisplay?: string;
+  bottomDisplay?:string;
+  bottomColor?:string;
 }
 
 const LenderBankCard: React.FunctionComponent<LenderBankCardProps> = ({
   name,
   image,
   small,
+  topElipseBottom,
+  topElipseright,
+  topElipseLeft,
+  topElipseTop,
+  bottomElipseBottom,
+  bottomElipseright,
+  bottomElipseLeft,
+  bottomElipseTop,
+  topColor,
+  topDisplay,
+  bottomDisplay,
+  bottomColor
 }) => {
+  console.log('image');
   return (
     <div className={small ? "bankCardWrapperContSmall" : "bankCardWrapperCont"}>
-      <div className="yellow">
-        <YellowEllipse />
+      <div className="yellow" style={{right:topElipseright,bottom:topElipseBottom, left:topElipseLeft,top:topElipseTop ,display:topDisplay}}>
+        <TopEllipse color={topColor} />
       </div>
-      <div className="blue">
-        <BlueEllipse />
+      <div className="blue" style={{right:bottomElipseright,bottom:bottomElipseBottom, left:bottomElipseLeft,top:bottomElipseTop ,display:bottomDisplay}}>
+        <BottomEllipse color={bottomColor} />
       </div>
       <div className={small ? "bankCardSmall" : "bankCard"}>
-        {image ? <img src={image} alt={name} /> : <p>{name}</p>}
+        {image ? <img src={image} className={small ? "bankImageSmall" : "bankImage"} alt={name} /> : <p>{name}</p>}
       </div>
     </div>
   );
